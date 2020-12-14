105a4200ade56f4641c359d6891263e851876468
overflowerhidden <1064155364@qq.com>
2020-12-14 14:47:39 +0800

    修改serve

--- serve/index.js
+++ serve/index.js
@@ -4,4 +4,6 @@ const app = express()
 app.get('/api/courses', (req, res) => {
     res.json([{ name: 'web全栈', price: 8999 }, { name: 'web高级', price: 8999 }])
 })
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('listion at 3000');
+})
