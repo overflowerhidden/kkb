a900eafab944c203d6c31264a2f8844f2aeb4a8b
overflowerhidden <1064155364@qq.com>
2020-12-14 14:57:16 +0800

    'show'

--- serve/index.js
+++ serve/index.js
@@ -5,5 +5,5 @@ app.get('/api/courses', (req, res) => {
     res.json([{ name: 'web全栈', price: 8999 }, { name: 'web高级', price: 8999 }])
 })
 app.listen(3000, () => {
-    console.log('listion at 3000');
+    console.log('listion at 3000 !!');
 })
